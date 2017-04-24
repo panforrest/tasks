d147b2d6a84eb55e50a641a9fc3f9120a1fa62db
panforrest <guoqianp@gmail.com>
2017-04-24 14:44:33 -0400

    03:02:07 create task on twilio sms

--- routes/twilio.js
+++ routes/twilio.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var router = express.Router()
+var controllers = require('../controllers')
 
 router.get('/task',function(req, res, next){
 	res.json({
@@ -11,7 +12,22 @@ router.get('/task',function(req, res, next){
 router.post('/task',function(req, res, next){
 	// res.render('index', {title: 'Express'})  //res.send('')
 	console.log('TWILIO: '+JSON.stringify(req.body))
-	res.send('Hello!')
+
+    var message = req.body['Body']
+    var task = {
+    	title: 'Twilio Task',
+    	category: 'delivery',
+    	description: message
+    }
+
+    controllers.task.post(task, false)
+    .then(function(result){
+    	console.log('SUCCESS: '+JSON.stringify(result))
+        res.send('Hello')
+    })
+    .catch(function(err){
+        console.log('ERROR: '+err)
+    }
 })
 
 
